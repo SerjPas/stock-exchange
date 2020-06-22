2a1ee00c5be652d742e7eb28a8d1acfc61634d88
Serhii Pas <passerhii@gmail.com>
2020-06-22 17:38:25 +0300

    Milestone 2
    1)function showResultOnPage changed. Now I am using DocumentFragment interface .
    2)In the project folder, create a new file called company.html - this where your browser will look for when you click a company link from the main page (index.html) - done
    3)In this page, you should extract the symbol string from the url (for example, if the user clicked a link for /company.html?symbol=GOOG, you should have a variable in your JS code with “GOOG” as a string. - done
    4)Then, get the company profile with the following endpoint: https://financialmodelingprep.com/api/v3/company/profile/${symbol} where symbol is the company symbol extracted from the query params -done. (endpoint I have got from API page instead of this one)
    5)Present the company information in the screen (no design provided, go wild), with the company image, name, description and link
    6) Present the company stock price, and changes in percentages - if the change is negative, the changes in percentages should be in light green, else in red.

---
+++ js/company.js
@@ -0,0 +1,54 @@
+let companyProfileStore = {};
+grabElements();
+
+function grabElements() {
+    companyProfileStore.img = document.querySelector("#company-img");
+    companyProfileStore.companyName = document.querySelector("#company-name");
+    companyProfileStore.description = document.querySelector("#description");
+    companyProfileStore.link = document.querySelector('#company-link');
+    companyProfileStore.stockPrice = document.querySelector('#stock-price');
+    companyProfileStore.stockChanges = document.querySelector('#stock-changes');
+    companyProfileStore.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+}
+
+(() => {
+    const extractFromUrl = (key) => {
+        let urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(key);
+    };
+    const createCompanyProfileUrl = () => {
+        const {apiKey} = companyProfileStore;
+        let symbol = extractFromUrl('symbol');
+        const myUrlWithParams = new URL(`https://financialmodelingprep.com/api/v3/profile/${symbol}`);
+        myUrlWithParams.searchParams.append('apikey', `${apiKey}`);
+        return myUrlWithParams;
+    }
+
+    async function callServer(SERVER_URL) {
+        const response = await fetch(SERVER_URL);
+        return response.json();
+    }
+
+    const insertData = async () => {
+        const {img, companyName, description, link, stockPrice, stockChanges} = companyProfileStore;
+        let profileUrl = createCompanyProfileUrl();
+        let data = await callServer(profileUrl);
+        for (let company of data) {
+            img.setAttribute("src", `${company.image}`);
+            companyName.innerText = company.companyName;
+            description.innerText = company.description;
+            link.innerText = company.website;
+            stockPrice.innerText = company.price;
+            console.log(company.changes);
+            if (company.changes < 0) {
+                stockChanges.innerText = company.changes + ' %';
+                stockChanges.style.color = '#90EE90';
+            } else {
+                stockChanges.innerText = company.changes + ' %';
+                stockChanges.style.color = 'red';
+            }
+        }
+    };
+    insertData();
+
+})();
