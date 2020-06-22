e460dfc5d722946c0cdd0e4963cdd7616a5a7dfe
Serhii Pas <passerhii@gmail.com>
2020-06-22 17:40:54 +0300

    milestone 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
+let StockExchangeStore = {};
+grabElements();
+
+function grabElements() {
+    StockExchangeStore.listOfResults = document.querySelector('#list-of-results');
+    StockExchangeStore.form = document.querySelector('#input-form');
+    StockExchangeStore.loader = document.querySelector('.loader');
+    StockExchangeStore.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+
+}
+
 (() => {
-    let StockExchangeStore = {};
-    grabElements();
 
-    function grabElements() {
-        StockExchangeStore.listOfResults = document.querySelector('#list-of-results');
-        StockExchangeStore.form = document.querySelector('#input-form');
-        StockExchangeStore.loader = document.querySelector('.loader');
+    const showElement = (element) => element.classList.remove("d-none");
 
-    }
-
-    const showElement = (element) => element.classList.remove('d-none');
-
-    const hideElement = (element) => element.classList.add('d-none');
+    const hideElement = (element) => element.classList.add("d-none");
 
     async function callServer(SERVER_URL) {
         const response = await fetch(SERVER_URL);
@@ -20,33 +22,28 @@
 
     const showResultOnPage = (array) => {
         const {listOfResults} = StockExchangeStore;
-        let ulHtml = '';
+        let fragment = new DocumentFragment();
         for (let newArr of array) {
-            ulHtml += `<li><a href="/company.html?symbol=${newArr.symbol}"> ${newArr.name}. ${newArr.symbol}</a></li>`;
+            let li = document.createElement('li');
+            li.innerHTML = `<a href="./company.html?symbol=${newArr.symbol}"> ${newArr.name}. ${newArr.symbol}</a>`
+            fragment.appendChild(li);
         }
-        listOfResults.innerHTML = ulHtml;
-    }
+        listOfResults.appendChild(fragment);
+    };
 
     async function callResultsFromServer(event) {
         event.preventDefault();
-        const {loader} = StockExchangeStore;
+        const {loader, apiKey} = StockExchangeStore;
         showElement(loader);
-        let myInput = document.querySelector('#input').value;
-        let apiKey = '76343d22c23b89e80be07772818ad437';
+        let myInput = document.querySelector("#input").value;
         const SERVER_URL = `https://financialmodelingprep.com/api/v3/search?query=${myInput}&limit=10&exchange=NASDAQ&apikey=${apiKey}`;
         let result = await callServer(SERVER_URL);
         showResultOnPage(result);
         hideElement(loader);
     }
 
-
     (() => {
         const {form} = StockExchangeStore;
         form.addEventListener("submit", callResultsFromServer);
-
     })();
-
-
 })();
-
-
