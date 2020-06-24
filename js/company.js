aa07ea74b54da4d7bb2cb19af6d7c50614b15b61
Serhii Pas <passerhii@gmail.com>
2020-06-24 09:28:53 +0300

    Milestone3 done
    1)Add extra information to search results - image and stock change (percentage) - done
    2) Use array.map (if didn’t do it already) when creating the li elements -done

--- js/company.js
+++ js/company.js
@@ -1,61 +1,88 @@
-let companyProfileStore = {};
-grabElements();
+let img = document.querySelector("#company-img");
+let companyName = document.querySelector("#company-name");
+let description = document.querySelector("#description");
+let link = document.querySelector('#company-link');
+let stockPrice = document.querySelector('#stock-price');
+let stockChanges = document.querySelector('#stock-changes');
+let apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+let loader = document.querySelector('.loader');
 
-function grabElements() {
-    companyProfileStore.img = document.querySelector("#company-img");
-    companyProfileStore.companyName = document.querySelector("#company-name");
-    companyProfileStore.description = document.querySelector("#description");
-    companyProfileStore.link = document.querySelector('#company-link');
-    companyProfileStore.stockPrice = document.querySelector('#stock-price');
-    companyProfileStore.stockChanges = document.querySelector('#stock-changes');
-    companyProfileStore.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+const showElement = (element) => element.classList.remove("d-none");
+const hideElement = (element) => element.classList.add("d-none");
+
+const extractFromUrl = (key) => {
+    let urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(key);
+};
+const createCompanyProfileUrl = () => {
+    let symbol = extractFromUrl('symbol');
+    return new URL(`https://financialmodelingprep.com/api/v3/profile/${symbol}?apikey=${apiKey}`);
 }
 
-(() => {
-    const extractFromUrl = (key) => {
-        let urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get(key);
-    };
-    const createCompanyProfileUrl = () => {
-        const {apiKey} = companyProfileStore;
-        let symbol = extractFromUrl('symbol');
-        const myUrlWithParams = new URL(`https://financialmodelingprep.com/api/v3/profile/${symbol}`);
-        myUrlWithParams.searchParams.append('apikey', `${apiKey}`);
-        return myUrlWithParams;
-    }
+async function callServer(SERVER_URL) {
+    const response = await fetch(SERVER_URL);
+    return response.json();
+}
 
-    async function callServer(SERVER_URL) {
-        const response = await fetch(SERVER_URL);
-        return response.json();
-    }
+const getStockHistory = async () => {
+    let symbol = extractFromUrl('symbol');
+    const SERVER_URL = `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?serietype=line&apikey=${apiKey}`;
+    let stockHistoryArray = await callServer(SERVER_URL);
+    return stockHistoryArray.historical;
 
-    const insertData = async () => {
-        const {img, companyName, description, link, stockPrice, stockChanges} = companyProfileStore;
-        let profileUrl = createCompanyProfileUrl();
-        let data = await callServer(profileUrl);
-        for (let company of data) {
-            img.setAttribute("src", `${company.image}`);
-            companyName.innerText = company.companyName;
-            description.innerText = company.description;
-            link.innerText = company.website;
-            stockPrice.innerText = company.price;
-            console.log(company.changes);
-            if (company.changes < 0) {
-                stockChanges.innerText = company.changes + ' %';
-                stockChanges.style.color = '#90EE90';
-            } else {
-                stockChanges.innerText = company.changes + ' %';
-                stockChanges.style.color = 'red';
-            }
+}
+
+const createAChart = async () => {
+    let array = await getStockHistory();
+    let ctxL = document.getElementById("myChart").getContext('2d');
+    let myLineChart = new Chart(ctxL, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: "Stock Price History",
+                data: [],
+                backgroundColor: ['rgba(105, 0, 132, .2)',],
+                borderColor: ['rgba(200, 99, 132, .7)',],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            responsive: true
         }
-    };
-    insertData();
-    (async () => {
-        const {apiKey} = companyProfileStore;
-        let symbol = extractFromUrl('symbol');
-        const SERVER_URL = `https://financialmodelingprep.com/api/v3/historical-price-full/
-        ${symbol}?serietype=line&apikey=${apiKey}`;
-        let data = await callServer(SERVER_URL);
-        console.log(data);
-    })();
+    });
+    for (let i = 30; i >= 0; i--) {
+        myLineChart.data.labels[i] = array[i].date;
+        myLineChart.data.datasets[0].data[i] = array[i].close;
+    }
+    myLineChart.update();
+}
+
+const insertData = async () => {
+    let profileUrl = createCompanyProfileUrl();
+    let data = await callServer(profileUrl);
+    for (let company of data) {
+        img.setAttribute("src", `${company.image}`);
+        companyName.innerHTML = `<a href="${company.website}"> ${company.companyName}</a>`;
+        description.innerText = company.description;
+        link.setAttribute('href', `${company.website}`);
+        let textnode = document.createTextNode(`${company.website}`);
+        link.appendChild(textnode);
+        stockPrice.innerText = 'Stock price:' + '$' + company.price;
+        if (company.changes < 0) {
+            stockChanges.innerText = `(${company.changes} %)`;
+            stockChanges.style.color = 'red' ;
+        } else {
+            stockChanges.innerText = `(${company.changes} %)`;
+            stockChanges.style.color ='#90EE90';
+        }
+    }
+   
+    createAChart();
+}
+(async () => {
+    showElement(loader);
+    await insertData();
+    hideElement(loader);
 })();
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
-let StockExchangeStore = {};
-grabElements();
-
-function grabElements() {
-    StockExchangeStore.listOfResults = document.querySelector('#list-of-results');
-    StockExchangeStore.form = document.querySelector('#input-form');
-    StockExchangeStore.loader = document.querySelector('.loader');
-    StockExchangeStore.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
-
-}
-
 (() => {
+    let StockExchangeStore = {};
+    grabElements();
+
+    function grabElements() {
+        StockExchangeStore.listOfResults = document.querySelector('#list-of-results');
+        StockExchangeStore.form = document.querySelector('#input-form');
+        StockExchangeStore.loader = document.querySelector('.loader');
+        StockExchangeStore.apiKey = "ed93f3e229380c530b7a0e7663f86b99";
+
+    }
 
     const showElement = (element) => element.classList.remove("d-none");
-
     const hideElement = (element) => element.classList.add("d-none");
 
     async function callServer(SERVER_URL) {
@@ -20,16 +18,38 @@ function grabElements() {
         return response.json();
     }
 
-    const showResultOnPage = (array) => {
+    function showResultOnPage(array) {
         const {listOfResults} = StockExchangeStore;
+        listOfResults.innerText = '';
         let fragment = new DocumentFragment();
-        for (let newArr of array) {
-            let li = document.createElement('li');
-            li.innerHTML = `<a href="./company.html?symbol=${newArr.symbol}"> ${newArr.name}. ${newArr.symbol}</a>`
-            fragment.appendChild(li);
-        }
+        console.log(array);
+        array.map((objectInsideArray) => {
+            let liTag = document.createElement('li');
+            let img = document.createElement('img');
+            img.src = `${objectInsideArray[0].image}`;
+            img.style.height = '80px';
+            img.style.width = '80px';
+            img.classList.add('mr-2');
+            let aTag = document.createElement('a');
+            let text = `${objectInsideArray[0].companyName}. (${objectInsideArray[0].symbol})`;
+            aTag.append(text);
+            aTag.href = `./company.html?symbol=${objectInsideArray[0].symbol}`;
+            aTag.target = `_blank`;
+            let span = document.createElement('span');
+            let stockChange = `(${objectInsideArray[0].changes})`;
+            span.append(stockChange);
+            if (objectInsideArray[0].changes < 0){
+                span.style.color = 'red' ;
+            } else {
+                span.style.color = '#90EE90';
+            }
+            span.classList.add('ml-2');
+
+            liTag.append(img, aTag, span);
+            fragment.appendChild(liTag);
+        });
         listOfResults.appendChild(fragment);
-    };
+    }
 
     async function callResultsFromServer(event) {
         event.preventDefault();
@@ -38,7 +58,12 @@ function grabElements() {
         let myInput = document.querySelector("#input").value;
         const SERVER_URL = `https://financialmodelingprep.com/api/v3/search?query=${myInput}&limit=10&exchange=NASDAQ&apikey=${apiKey}`;
         let result = await callServer(SERVER_URL);
-        showResultOnPage(result);
+        const companyData = await Promise.all(result.map(async (company) => {
+            let url = `https://financialmodelingprep.com/api/v3/profile/${company.symbol}?apikey=${apiKey}`;
+            company.additionalResult = await callServer(url);
+            return callServer(url);
+        }));
+        showResultOnPage(companyData);
         hideElement(loader);
     }
 
@@ -46,4 +71,5 @@ function grabElements() {
         const {form} = StockExchangeStore;
         form.addEventListener("submit", callResultsFromServer);
     })();
+
 })();
